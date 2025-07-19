635a218ccca10ed76024de443e78735efcd29026
dungvv1.ho <dungvv1.ho@vietcombank.com.vn>
2025-07-19 19:32:25 +0700

    change directory path, add ./

--- index_support.js
+++ index_support.js
@@ -1,15 +1,15 @@
-import CalculateTree from "/src/CalculateTree/CalculateTree.js"
-import createStore from "/src/createStore.js"
-import view from "/src/view/view.js"
-import createSvg from "/src/view/view.svg.js"
-import * as handlers from '/src/handlers.js'
-import * as elements from '/src/elements.js'
-import * as htmlHandlers from '/src/view/view.html.handlers.js'
-import * as icons from '/src/view/elements/Card.icons.js'
-import createChart from '/src/createChart.js'
+import CalculateTree from "./src/CalculateTree/CalculateTree.js"
+import createStore from "./src/createStore.js"
+import view from "./src/view/view.js"
+import createSvg from "./src/view/view.svg.js"
+import * as handlers from './src/handlers.js'
+import * as elements from './src/elements.js'
+import * as htmlHandlers from './src/view/view.html.handlers.js'
+import * as icons from './src/view/elements/Card.icons.js'
+import createChart from './src/createChart.js'
 
-import CardSvg from '/src/Cards/CardSvg.js'
-import CardHtml from '/src/Cards/CardHtml.js'
+import CardSvg from './src/Cards/CardSvg.js'
+import CardHtml from './src/Cards/CardHtml.js'
 
 export default {
   CalculateTree,
